3bccf85191b46b081abe9042ecfdb77007b5a909
Bouba10 <72755006+Bouba10@users.noreply.github.com>
2023-10-26 10:39:51 +0200

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Boubacar sidiki barry on 26.10.23.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
